a684c49a601e9b0fc93c00692165fc70ea1517f0
mohamad <yusha.9ine@gmail.com>
2020-09-04 10:58:29 +0430

    jdbcTemplate, Flyway, Postgres, Docker

--- src/main/java/com/example/demo/dao/PersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/PersonDataAccessService.java
@@ -1,6 +1,8 @@
 package com.example.demo.dao;
 
 import com.example.demo.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -8,7 +10,14 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository("postgres")
-public class PersonDataAccessService implements PersonDao{
+public class PersonDataAccessService implements PersonDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public int insertPerson(UUID id, Person person) {
         return 0;
@@ -16,12 +25,24 @@ public class PersonDataAccessService implements PersonDao{
 
     @Override
     public List<Person> selectAll() {
-        return List.of(new Person(UUID.randomUUID(), "from Postgres"));
+        final String sqlQuery = "select * from person";
+        return jdbcTemplate.query(sqlQuery, (resultSet, i) -> {
+            return new Person(UUID.fromString(resultSet.getString("id")),
+                    resultSet.getString("name"));
+        });
     }
 
     @Override
     public Optional<Person> selectPersonById(UUID id) {
-        return Optional.empty();
+        final String sqlQuery = "select * from person where id = ?";
+        Person person = jdbcTemplate.queryForObject(
+                sqlQuery,
+                new Object[]{id},
+                (resultSet, i) -> {
+                    return new Person(UUID.fromString(resultSet.getString("id")),
+                            resultSet.getString("name"));
+                });
+        return Optional.ofNullable(person);
     }
 
     @Override
